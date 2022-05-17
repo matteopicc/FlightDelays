30028943fbd7f9dad0cf1ce0cb60f95c4f3311eb
Matteo <Matteo@ASUS-7812A.lan>
2022-05-17 09:07:38 +0200

    esercizio flightdelays in classe

--- src/main/java/it/polito/tdp/extflightdelays/FXMLController.java
+++ src/main/java/it/polito/tdp/extflightdelays/FXMLController.java
@@ -1,8 +1,10 @@
 package it.polito.tdp.extflightdelays;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import it.polito.tdp.extflightdelays.model.Airport;
 import it.polito.tdp.extflightdelays.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -28,10 +30,10 @@ public class FXMLController {
     private TextField compagnieMinimo; // Value injected by FXMLLoader
 
     @FXML // fx:id="cmbBoxAeroportoPartenza"
-    private ComboBox<?> cmbBoxAeroportoPartenza; // Value injected by FXMLLoader
+    private ComboBox<Airport> cmbBoxAeroportoPartenza; // Value injected by FXMLLoader
 
     @FXML // fx:id="cmbBoxAeroportoDestinazione"
-    private ComboBox<?> cmbBoxAeroportoDestinazione; // Value injected by FXMLLoader
+    private ComboBox<Airport> cmbBoxAeroportoDestinazione; // Value injected by FXMLLoader
 
     @FXML // fx:id="btnAnalizza"
     private Button btnAnalizza; // Value injected by FXMLLoader
@@ -41,11 +43,25 @@ public class FXMLController {
 
     @FXML
     void doAnalizzaAeroporti(ActionEvent event) {
-
+      txtResult.clear();
+      int x;
+      try {
+    	 x=Integer.parseInt(compagnieMinimo.getText()) ;
+      }catch(NumberFormatException e){
+    	  txtResult.appendText("Inserire valore numerico");
+    	  return;
+      }
+      this.model.creaGrafo(x);
+      cmbBoxAeroportoPartenza.getItems().addAll(this.model.getVertici());
+      cmbBoxAeroportoDestinazione.getItems().addAll(this.model.getVertici());
+      
+    	
     }
 
     @FXML
     void doTestConnessione(ActionEvent event) {
+    	List<Airport> percorso = this.model.getPercorso(cmbBoxAeroportoPartenza.getValue(), cmbBoxAeroportoDestinazione.getValue());
+    	System.out.println(percorso);
 
     }
 
